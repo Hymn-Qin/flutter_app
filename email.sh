34c808de72259d3c861fc8445d18254bf5287a50
秦小杰 <18339989537@163.com>
2019-08-11 22:14:42 +0800

    add

---
+++ email.sh
@@ -0,0 +1,19 @@
+﻿#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="xiaojie.qin@geely.com"
+CORRECT_NAME="秦小杰"
+CORRECT_EMAIL="18339989537@163.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
